ed3c0792c9d7a2a8ac516fd2b0fea595d6c3e1f6
Ricky JW <rickywid@hotmail.com>
2016-04-04 17:44:46 -0400

    update chart details

--- js/chartist/chartist-custom.js
+++ js/chartist/chartist-custom.js
@@ -1,7 +1,7 @@
 new Chartist.Bar('.ct-chart', {
   labels: ['Wordpress', 'Git', 'Ruby on Rails', 'Photoshop', 'React', 'jQuery', 'Javascript', 'Responsive', 'CSS', 'HTML'],
   series: [
-    [3.7, 5, 4.5, 8, 5.2, 6.5, 7.4, 8, 8.8, 9],
+    [3.5, 5.1, 5.2, 8, 5.2, 6, 7.4, 8.4, 9.5, 9.5],
   ]
 }, {
 
