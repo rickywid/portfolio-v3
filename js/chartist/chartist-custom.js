4bb0e94956474a5c21331c3caab67c24ab931318
Ricky JW <rickywid@hotmail.com>
2016-04-04 17:32:11 -0400

    add chart

--- js/chartist/chartist-custom.js
+++ js/chartist/chartist-custom.js
@@ -1,7 +1,7 @@
 new Chartist.Bar('.ct-chart', {
   labels: ['Wordpress', 'Git', 'Ruby on Rails', 'Photoshop', 'React', 'jQuery', 'Javascript', 'Responsive', 'CSS', 'HTML'],
   series: [
-    [3, 5, 3.5, 8, 4, 6.5, 7, 8, 9, 9],
+    [3.5, 5, 4, 8, 3.8, 6.5, 7.4, 8, 8.8, 9],
   ]
 }, {
 
